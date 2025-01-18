5a2aecfbc714d16c97238f2b261901b01907770a
demgerall <demgerall@mail.ru>
2025-01-18 19:22:52 +0700

    Add basic page and route (configured react-router-dom)

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import { AppRoutes } from './routes/Routes';
+import { Header } from '@/widgets';
+
 export const App = () => {
-    return <div>App</div>;
+    return (
+        <>
+            <Header />
+            <AppRoutes />
+        </>
+    );
 };

d48e0cda23b48a1f555af6717e80d048aa7ffac0
demgerall <demgerall@mail.ru>
2025-02-04 19:57:35 +0700

    Fix #33

--- src/shared/routes/routes.ts
+++ src/shared/routes/routes.ts
@@ -1,5 +1,5 @@
 export const ROUTES = {
-    HOME: '/My_Resume_Site_v2/',
-    ALL_PROJECTS: '/My_Resume_Site_v2/projects',
-    PROJECT: '/My_Resume_Site_v2/projects/:id',
+    HOME: '/',
+    ALL_PROJECTS: '/projects',
+    PROJECT: '/projects/:id',
 };

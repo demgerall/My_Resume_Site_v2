9b24d247e55118f1fb2d1eb32197c30ef0b36201
demgerall <demgerall@mail.ru>
2025-02-04 18:57:21 +0700

    Fixing deploy problems

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import { App } from '@/App/index';
 import { store } from '@/App/store/store';
 
 import '@/App/styles/index.scss';
-import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
